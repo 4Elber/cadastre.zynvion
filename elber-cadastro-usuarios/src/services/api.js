3f5ef3081d0c86373e2c8066ce290fb0a51854c6
Elber Lucas Alves Barboza <142125443+4Elber@users.noreply.github.com>
2025-03-28 22:18:00 -0400

    update

--- elber-cadastro-usuarios/src/services/api.js
+++ elber-cadastro-usuarios/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://cadastre-zynvion.onrender.com/users'
+    baseURL: 'https://cadastre-zynvion.onrender.com'
 })
 
 export default api
